b5bfd1a8f032c1325ac224fbcac4f7cbb6ac8ace
ayveezub <ayveezub@gmail.com>
2024-06-04 18:20:59 +0300

    feat(gendiff.js): add command line option -f (--format)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>');
+
+program
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
