9da612f6d4e22f9423375218df48e2e6803b9062
ayveezub <ayveezub@gmail.com>
2024-06-05 17:12:07 +0300

    feat: update gendiff.js to accept paths to files as arguments and add reading and parsing JSON files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/main.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .arguments('<filepath1> <filepath2>');
+  .version('0.0.3');
 
 program
-  .option('-f, --format [type]', 'output format');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((first, second) => {
+    const diff = genDiff(first, second);
+
+    console.log(diff);
+  });
 
 program.parse();
--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-export default () => {};
+import { resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
+import * as parse from './parsing.js';
+
+const isJSON = (filepath) => filepath.endsWith('.json');
+
+export default (filepath1, filepath2) => {
+  const absolutePath1 = resolve(filepath1);
+  const absolutePath2 = resolve(filepath2);
+
+  const encoding = 'utf-8';
+  const text1 = readFileSync(absolutePath1, encoding);
+  const text2 = readFileSync(absolutePath2, encoding);
+
+  let data1;
+  let data2;
+  if (isJSON(filepath1)) {
+    data1 = parse.json(text1);
+  }
+  if (isJSON(filepath2)) {
+    data2 = parse.json(text2);
+  }
+
+  return [data1, data2];
+};
